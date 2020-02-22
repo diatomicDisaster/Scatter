48f9ba05a7959e0bf209c1903d51b8bb4fa5557d
diatomicDisaster <wilfrid.somogyi.15@ucl.ac.uk>
2020-02-22 14:11:59 +0000

    Minor additions

--- scatter.py
+++ scatter.py
@@ -1,84 +1,67 @@
-import numpy as np
+from numpy.random import uniform, normal
+from numpy import multiply, array, pi, floor, inner, sin, cos, float64, append, empty, zeros
+
+# Dev libs
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from time import process_time
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-Nx = 5
-Ny = 5
-Nz = 5
-size = np.array([Nx, Ny, Nz])
+gridSize = array([100, 100, 100])
+stopLen = 50.
+maxSteps = 1000
 
-Lx = 47.893
-Ly = Lx
-Lz = 1
-nMols = 2
-
-# Dummy routine for assigning abundance to cells
-def returnNum(z, nTot):
-    A1 = z*z
-    return A1*nTot
-cells = np.zeros((Nx, Ny, Nz), dtype=np.float64)
-for i in range(Nx):
-    for j in range(Ny):
-        for k in range(Nz):
-            z = k * Lz/Nz
-            cells[i][j][k] = returnNum(z, z*1e6)
-
-# Random initial position and angle
-initAng = np.random.uniform(size=2)*np.pi
-initPos = np.random.uniform(size=2)
-initCell = np.append(np.floor(np.multiply(initPos, size[:2])), 0)
-
-# Path length
-tau = 5
-
-# Garbage that needs cleaning
-dead = False
-sumPath = 0.
-r0Vec = np.array([2.5, 3.5, 0.])
-dVec  = np.array([1., 1., 1.])
-dVec  = dVec / np.linalg.norm(dVec)
-points = []
-
-while not dead:
-    path = float('inf')
-    thisCell = np.floor(r0Vec) # x, y, z indices of current cell
-    nextCell = thisCell + 1 # next (x+1, y+1, z+1) cell
-    #unitVec = rNow/np.linalg.norm(rNow) # unit vector for current direction
-    # need two p0 points to specify 6 planes - (0, 0, 0) and (1, 1, 1)
-    for p0Ind, p0Vec in enumerate([thisCell, nextCell]):
-        # and three unit vectors to specify 6 planes
-        for normInd, norm in enumerate(np.array([[1, 0, 0], 
-                                                [0, 1, 0], 
-                                                [0, 0, 1]])):
-            # calculate path length until plane intercept for all 6 planes
-            t = np.inner((p0Vec - r0Vec), norm)/np.inner(dVec, norm)
-            # find the first plane we pass through - i.e when we exit the cell
-            if ((abs(t) < path) and (t > 0.0)):
-                sumPath += t
-                path = t
-                # index the cell wall by it's normal vector and p0
-                wall = [normInd, p0Ind]
-    # save points for plotting
-    points.append(r0Vec)
-    # update position
-    r0Vec = r0Vec + path*dVec
-    # check path length
-    if sumPath > 5:
-      dead = True
-
-# Plotting to check results
-xs = np.array([point[0] for point in points])
-ys = np.array([point[1] for point in points])
-zs = np.array([point[2] for point in points])
-ax.scatter(xs, ys, zs)
-X, Y = np.meshgrid(np.arange(0, Nx+1, 1), np.arange(0, Ny+1, 1))
-for k in range(Nz+1):
-    Z = np.full((Nx+1, Ny+1), k)
-    ax.plot_wireframe(X, Y, Z, lw=0.1)
-
-ax.set_xlim(0, Nx)
-ax.set_ylim(0, Ny)
-ax.set_zlim(0, Nz)
+# Loop over incident photons
+for particle in range(10):
+    plotPoints = []
+    # Random x, y position (z=0) and propagation angle (theta < pi/2)
+    posVec  = multiply(array([*uniform(size=2), 0]), gridSize)
+    randAng = multiply(uniform(size=2), array([pi/2, 2*pi]))
+    propVec = array([sin(randAng[0])*cos(randAng[1]), 
+                    sin(randAng[0])*sin(randAng[1]),
+                    cos(randAng[0])])
+    # Initialise path length, begin propagation
+    sumPath = 0.
+    inGrid = True
+    nStep = 0
+    while inGrid and nStep < maxSteps:
+        nStep += 1
+        plotPoints.append(posVec.copy())
+        thisCell = floor(posVec) # Retrieve cell x, y, z indices
+        topCell  = thisCell +1 # Retrieve n + 1 cell
+        # Find cell exit point
+        cellPath = float('inf')
+        for pointInd, wallPoint in enumerate([thisCell, topCell]):
+            for normInd in range(3):
+                normVec = zeros(3, dtype=float64)
+                normVec[normInd] = 1.
+                # Calculate distance to exit on nth cell wall 
+                exitLen = inner((wallPoint - posVec), normVec) \
+                    / inner(propVec, normVec)
+                # Choose shortest exit length in forward direction
+                if (exitLen > 0.) and (exitLen < cellPath):
+                    cellPath = exitLen
+                    exitWall = [pointInd, normInd]
+        # If total path length exceeds scatter length: scatter
+        if sumPath + cellPath > stopLen:
+            cellPath -= sumPath + cellPath - stopLen
+            randAng   = multiply(uniform(size=2), array([pi, 2*pi]))
+            posVec   += cellPath * propVec
+            propVec   = array([sin(randAng[0])*cos(randAng[1]), 
+                               sin(randAng[0])*sin(randAng[1]),
+                               cos(randAng[0])])
+            sumPath = 0.
+        else:
+            sumPath += cellPath
+            posVec  += cellPath*propVec
+        if not all([0 <= thisCell[i] <= gridSize[i] for i in range(3)]):
+            inGrid = False
+    points = array(plotPoints).T
+    ax.plot(points[0], points[1], points[2])
+ax.set_xlim(0, gridSize[0])
+ax.set_ylim(0, gridSize[1])
+ax.set_zlim(0, gridSize[2])
+plt.savefig('traces.png')
 plt.show()
+
