d3230f608808ad40324e2a054f036ad74e3e3bf8
diatomicDisaster <wilfrid.somogyi.15@ucl.ac.uk>
2020-03-05 18:59:38 +0000

    Implemented changes for calculating weights and intensity.

--- scatterer.py
+++ scatterer.py
@@ -8,20 +8,24 @@ start_time = time.time()
 # Possibly needs to become an inherited class to
 # allow for different phase functions. Need to think about this.
 class Molecule:
-    def __init__(self, name="no name", phase_g=0.5):
+    def __init__(self, name="no name", phase_SD=0.5):
         """A class to define molecules with different width phase functions
         
         Inputs
         name : str
             A name for the molecule, default 'no name'.
-        phase_g : float
+        phase_SD : float
             Standard deviation (radians) of the molecule's phase function.
         """
         self.name = name
-        self.phase_g = phase_g
+        self.phase_SD = phase_SD
 
-    def phase_func(self):
-        """Gaussian scattering phase function.
+    def phase_func(self, theta):
+        """Returns probability of scattering at angle theta."""
+        return 2*np.exp(-theta/2*self.phase_SD**2) # x2 because theta > 0
+
+    def inv_phase_func(self, rand_state=np.random):
+        """Draws random theta, phi from Gaussian scattering phase function.
         Returns
             theta : float
                 A random azimuthal angle theta > 0 from a normal 
@@ -30,8 +34,8 @@ class Molecule:
                 A random polar angle phi from a uniform
                 distribution in the range [0, 2*pi].
         """
-        theta = np.abs(np.random.normal(loc=0.0, scale=self.phase_g))
-        phi = np.random.uniform()*2*np.pi
+        theta = np.abs(rand_state.normal(loc=0.0, scale=self.phase_SD))
+        phi = rand_state.uniform()*2*np.pi
         return theta, phi
 
     def z_abund_func(self, z):
@@ -42,7 +46,7 @@ class Molecule:
         Returns
             A(z) : float
                 The relative abundance at height z. """
-        return 0.1 #np.exp(-z) 
+        return 5e-2 #np.exp(-z) 
 
 class Grid:
     def __init__(self, grid_size=(100,100,100), molecules=['arb']):
@@ -66,6 +70,8 @@ class Grid:
             for molecule in self.molecules:
                 self.abs_grid[:, :, i] += molecule.z_abund_func(z)
         self.abs_grid = self.abs_grid/len(self.molecules)
+        self.sca_grid = self.abs_grid
+        self.sum_grid = self.abs_grid + self.sca_grid
 
 class Scatter:
     def __init__(self, grid):
@@ -77,7 +83,7 @@ class Scatter:
         """
         self.grid = grid
 
-    def propagate(self, max_step=10000, rand_seed=None): #changed max step from 999 to 10000
+    def propagate(self, max_step=999, rand_seed=None, nu=None):
         """Propagate a photon through the grid.
         
         Inputs
@@ -85,22 +91,28 @@ class Scatter:
                 Maximum number of path steps to make before giving up.
             rand_seed : int
                 Seed to generate reproducible random numbers.
-                Used for initial position and tau value.
+                Used for initial position and propagation vector.
+            nu : float
+                Wavenumber of photon to propagate.
                 
         """
-        #Lara: should we add beta_sca param to the arguments for this funciton? for now we will take a homogeneous approach and use 
-        # beta_sca_i = 0.01 for each molecule type i. then beta_sca for a cell is the sum of these over the number of types, lets assume 
-        # ten types of molecule and the same in every cell. then beta_sca = 0.1
-        beta_sca = 0.1
         self._max_step   = max_step
         self._rand       = np.random.RandomState(rand_seed)
+        self.weights     = [[],[]]
         
+        # Random initial x, y position (z=0) and propagation vector
         rand = self._rand.uniform(size=(2,2))
         self.pos_vec     = np.full((max_step+1, 3), np.nan)
         self.pos_vec[0]  = np.append(rand[0], 0.) * self.grid.grid_size
-        self.prp_vec     = self._pol_to_cart(np.insert(rand[1]*np.array([.25, 2.])*np.pi,  0, 1.))
-        self.max_tau     = -np.log(1 - (self._rand.uniform())/beta_sca) # integrated path length. LARA: need to scale second term in log by 1/beta_sca, which is a function of the cell!! :)
-        self.sum_tau     = 0.
+        self.prp_vec     = self._pol_to_cart(
+            np.insert(rand[1]*np.array([.25, 2.])*np.pi,  0, 1.))
+        
+        ## Wilf: you had -np.log(1 - rho/beta_scat) but I'm not sure this makes
+        ##       sense. That function is only defined for beta_scat>rho, so generates
+        ##       infinite path lengths in many cases.
+        # Initialise random optical depth integrated path length for first step
+        self.max_abs     = -np.log(1 - self._rand.uniform()) 
+        self.sum_abs     = 0.
         
         self.left_grid = False
         for n_step in range(max_step):
@@ -121,23 +133,27 @@ class Scatter:
                     # shortest positive intercept distance is exit point
                     if 0. < test_len < stop_len:
                         stop_len = test_len
-            #beta_sca   = self.grid.abs_grid[tuple(self._cell)]
-            self.sum_tau += stop_len*beta_sca
-            # check whether max_tau is reached along this path
-            if self.sum_tau >= self.max_tau:
-                stop_len = (self.sum_tau - self.max_tau)/beta_sca
-                self.sum_tau = self.max_tau
+            beta_abs   = self.grid.abs_grid[tuple(self._cell)]
+            self.sum_abs += stop_len*beta_abs
+            # check whether max_abs is reached along this path
+            if self.sum_abs >= self.max_abs:
+                stop_len = (self.sum_abs - self.max_abs)/beta_abs
+                self.sum_abs = self.max_abs
             self.pos_vec[n_step + 1] = self.pos_vec[n_step] + stop_len*self.prp_vec
-            if self.sum_tau is self.max_tau:
-
-            
-
-                self._scatter()
-        #Lara: want to store an aborption weight after each scatter order. 
-        # this is the proability that this path has been traversed by a photon before absorption.
-        self.w_abs_order = np.exp(-stop_len*beta_sca) #stop_len or test_len ?? Want this length to be length of path traversed only in this order - NOT from zero.
-        self.w_abs += self.w_abs_order # we want to work out w_abs_order for each scattering order and then add this to the running propagation
-        # (only for absoprtion not scattering weight).
+            if self.sum_abs is self.max_abs:
+                theta = self._scatter()
+                self._detect(theta, self.pos_vec[n_step+1])
+                # New random optical depth and zero integrated path length
+                self.max_abs = -np.log(1 - self._rand.uniform())
+                self.sum_abs = 0.
+        ## Wilf: Currently we get values of I_p > 1, is this right?
+        ##       Maybe there needs to be some form of normalisation somewhere,
+        ##       e.g dividing by the number of scattering orders or total path length?
+        # Sum of weight products over scatter orders
+        if len(self.weights[0]) == 0 and len(self.weights[1]) ==0:
+            self.intens_p = "no weights" # means photon left grid before scattered
+        else:
+            self.intens_p = np.dot(self.weights[0], self.weights[1])
     
     def _pol_to_cart(self, pol_vec):
         """Convert vector from polar (r, theta phi) to Cartesian (x, y, z)"""
@@ -156,11 +172,30 @@ class Scatter:
         elif cart_vec[0] < 0:
             phi = phi + np.pi
         return np.array([r, theta, phi])
-        
+
+    def _detect(self, theta, pos_vec):
+        """Calculate weights for scattering and absorption paths at current scatter order.
+        Inputs
+            theta : float
+                Phase function angle 0 <= theta <= pi.
+            pos_vec : float, array-like
+                Position vector of scattering event.
+        """
+        # Sum of absorption and scattering along path to detector
+        detect_path  = ((self._cell[2] + 1) - pos_vec[2]) \
+            * self.grid.sum_grid[tuple(self._cell)] \
+            + np.sum(self.grid.sum_grid[self._cell[0], 
+                                        self._cell[1], 
+                                        self._cell[2]+1:])
+        ## Wilf: which molecule's phase function do we use below?
+        P = self.grid.molecules[0].phase_func(theta) # phase function probability
+        self.weights[1].append(np.exp(-detect_path)*P) # scattering weight
+        self.weights[0].append(np.exp(-self.sum_abs)) # absorption weight
+
     def _scatter(self):
-        """Scatter the photon from a molecule following some phase function"""
-        rand_mol  = np.random.randint(0, high=len(self.grid.molecules))
-        theta_, phi_ = self.grid.molecules[rand_mol].phase_func()
+        """Scatter the photon from a molecule following some phase function."""
+        rand_mol  = self._rand.randint(0, high=len(self.grid.molecules))
+        theta_, phi_ = self.grid.molecules[rand_mol].inv_phase_func(rand_state=self._rand)
         new_prp_ = self._pol_to_cart(np.array([1., theta_, phi_]))
         r, theta, phi = self._cart_to_pol(self.prp_vec)
         Ry = np.array([[np.cos(theta), 0., np.sin(theta)],
@@ -171,25 +206,7 @@ class Scatter:
                        [0., 0., 1.]])
         new_prp = np.dot(Rz, np.dot(Ry, new_prp_))
         self.prp_vec = new_prp
-        self.sum_tau = 0.
-        
-        ## This is the probability of no scatter+absorption along the path traversed from this 
-        ## point up until the detector.
-        P = phase_func(theta_, phi_)
-        theta_d = 0 ## this is the angle of the incoming radiation beam to the detector. since we're assuming up-down/left-right propagation, for now set to 0.
-        z_length =    # cartesian distance from this scatter order to the detector along z-axis.
-        beta_sca_to_det =  f(z_length) #sum of beta_sca_j for molecules j in cells travered to det. Assume homog. let beta_sca_j=0.01 then z_length determines no. of cells.
-        beta_abs_to_det =  g(z_length) #sum of beta_abs_j for molecules j in cells travered to det. Assume homog. let beta_abs_j=0.01 (?) then z_length determines no. of cells.
-
-        w_sca = P*np.exp(-z_length*(beta_abs_to_det + beta_sca_to_det))/np.cos(theta_d) 
-
-        # finally to work out intensity contribution for one photon, at each scattering order we take product of weights
-        N_p = # number of photons so argument of range in "for i in range"
-        I_photon = sum_over_orders{w_sca*w_abs}
-        I = (1/N_p)*sum_over_photons{I_photon} # gives intensity averaged over photons for one wavelength.
-
-
-        return self
+        return theta_
 
 # A dictionary of molecules
 moleculeDict = {
@@ -213,16 +230,25 @@ gridA = Grid(grid_size=(100,100,100))
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-# Perform 100 scatter instances on the grid and plot. Lara: you mean 100 photons not scattering orders??
+# Invoke 100 instances of 'Scatter' on the grid and plot.
 scatters = []
-for i in range(1000):
+nl = 'No scattering'
+for i in range(50):
     scatters.append(Scatter(gridA))
     scatters[i].propagate()
+    p = scatters[i].intens_p
     points = scatters[i].pos_vec.T
-    ax.plot(points[0], points[1], points[2])
+    if p == "no weights":
+        ax.plot(points[0], points[1], points[2], c='red', label=nl)
+        nl = '_nolegend_'
+    else:
+        ax.plot(points[0], points[1], points[2], c='blue')
 ax.set_xlim(0, gridA.grid_size[0])
 ax.set_ylim(0, gridA.grid_size[1])
 ax.set_zlim(0, gridA.grid_size[2])
+ax.set_zlabel('Atmospheric Depth (arb. units)')
+ax.invert_zaxis()
+plt.legend()
 plt.savefig('traces2.png')
 print("--- %s seconds ---" % (time.time() - start_time))
 plt.show()
