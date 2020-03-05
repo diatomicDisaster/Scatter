5b43455cd26115067d306ff40ad57b7ef4cf96f9
laraanisman <58424003+laraanisman@users.noreply.github.com>
2020-03-05 11:31:24 +0000

    hello. thoughts on weights :)

--- scatterer.py
+++ scatterer.py
@@ -75,16 +75,22 @@ class Scatter:
         self.grid = grid
 
     def propagate(self, max_step=999, rand_seed=None):
+        ## Lara: could we possibly add the beta_sca parameter in the arguments here? It will be the sum of all the beta_sca_j for 
+        # molecule types j in the cells that are propagated through. For now we can take this to be homogeneous just to get it running! 
+        # So let's say beta_sca_j = 0.01 for 10 types of molecule --> beta_sca = 0.1
+        beta_sca = 0.1
         """Propagate a photon through the grid.
         
         Inputs
             max_step : int
-                Maximum number of path steps to make before giving up.
+                Maximum number of path steps to make before giving up. 
             rand_seed : int
                 Seed to generate reproducible random numbers.
                 Used for initial position and tau value.
                 
         """
+        ##Lara: I wonder if we should let the paths run and only stop them when they reach the top of the z axis?? as opposed to max step.
+
         self._max_step   = max_step
         self._rand       = np.random.RandomState(rand_seed)
         
@@ -92,9 +98,9 @@ class Scatter:
         self.pos_vec     = np.full((max_step+1, 3), np.nan)
         self.pos_vec[0]  = np.append(rand[0], 0.) * self.grid.grid_size
         self.prp_vec     = self._pol_to_cart(np.insert(rand[1]*np.array([.25, 2.])*np.pi,  0, 1.))
-        self.max_tau     = -np.log(1 - self._rand.uniform()) # draw a random integrated optical path length [0, infty]
+        self.max_tau     = -np.log(1 - (self._rand.uniform())/beta_sca) # draw a random integrated optical path length [0, infty]
         self.sum_tau     = 0. # summation of traversed optical path length
-        
+
         self.left_grid = False
         for n_step in range(max_step):
             self._cell = np.floor(self.pos_vec[n_step]).astype(int) # x,y,z indices of cell
@@ -116,14 +122,26 @@ class Scatter:
                     # shortest positive intercept distance is exit point
                     if 0. < test_len < stop_len:
                         stop_len = test_len
-            beta_ext   = self.grid.abs_grid[tuple(self._cell)]  # integrate beta along path length
-            self.sum_tau += stop_len*beta_ext # add to summation
+            #beta_sca   = self.grid.abs_grid[tuple(self._cell)]  # integrate beta along path length
+            
+            self.sum_tau += stop_len*beta_sca # add to summation
             if self.sum_tau >= self.max_tau: # check if max_tau is reached along this path
-                stop_len = (self.sum_tau - self.max_tau)/beta_ext # shorten stop length
+                stop_len = (self.sum_tau - self.max_tau)/beta_sca # shorten stop length
                 self.sum_tau = self.max_tau
             self.pos_vec[n_step + 1] = self.pos_vec[n_step] + stop_len*self.prp_vec
             if self.sum_tau is self.max_tau: # scatter if scattering optical depth reached
                 self._scatter()
+
+
+            ## Lara: store absorption weight! :)
+            # do i need to initialise both of these as empty arrays?? 
+            self.w_abs_this_step = np.exp(-stop_len*beta_sca)
+            # needs:
+            # length_in_cell = individual cell lengths traveresed x the beta_sca param in each cell
+            # BUT FOR NOW we can just use stop_len multiplied by beta_sca in cells traversed so far?? need to check that stop_len is free length travelled and not starting from zero! 
+            # Then we cumulate previous path weights to obtain an aborption weight for each scattering order:
+            self.w_abs += self.w_abs_this_step # lol is this even right ??
+
     
     def _pol_to_cart(self, pol_vec):
         """Convert vector from polar (r, theta phi) to Cartesian (x, y, z)"""
@@ -159,8 +177,28 @@ class Scatter:
         new_prp = np.dot(Rz, np.dot(Ry, new_prp_)) # transform k' from k=z to global Cartesian frame
         self.prp_vec = new_prp
         self.sum_tau = 0.
-        return self
 
+        #Lara: store scattering weight :) 
+        ## This is the probability of no scatter+absorption along the path traversed from this 
+        ## point up until the detector.
+        P = phase_func(theta_, phi_)
+        theta_d = 0 ## this is the angle of the incoming radiation beam to the detector. since we're assuming up-down/left-right propagation, for now set to 0.
+        z_length =      # the cartesian length of the path straight to the detector _before_ we scatter. total distance to det - distance weve travelled in total so far!! 
+        beta_abs_to_det = f(z_length)    # the absorption coefficients summed over the cells traversed now _to_ the detector. lets for now put chosen number. z_length would determine no. of cells. 
+        beta_sca_to_det = g(z_length)   # the scattering coefficients summed over the cells traversed now _to_ the detector. lets for now put chosen number. z_length would determine no. of cells. 
+
+        self.w_sca = P*np.exp(-z_length*(beta_abs_to_det + beta_sca_to_det))/np.cos(theta_d) 
+
+        # Now want to, at each scattering order, store the value of w_abs and w_sca, and multiply together to obtain intensity contribution at this step.
+        # When we reach the end of the grid, so the location of the detector, we will sum all of these values to get intensity contribution for all possible paths. 
+        # Then divide by the number of photons - argument of "for i in range", call it N_p - to obtain averaged intensity for this wavelength. YAY
+
+         I_order = self.w_sca*self.w_abs # this is for ONE photon at EACH scattering order. 
+         I_photon = sum_total_orders{I_order}     # Maybe these three lines come out of iteration? quite sure this isnt implemented correctly lol 
+         I = (1/N_p)*sum_photons{I_photon}      # Not sure if this is making sense ??
+
+        return self
+         
 # A dictionary of molecules
 moleculeDict = {
     "arb": Molecule(name = "arbitrary") # arbitrary molecules
